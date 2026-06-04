0a6aa8f6e8e0fac769dc321b2325ad35ebbd1b44
Ratan Mahata <ratanmahata116@gmail.com>
2026-06-04 23:28:44 +0530

    signup logic working

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,20 @@
-
-
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Signup from "./pages/Signup"
 
 function App() {
 
 
   return (
     <div>
-      hello jee
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/signup"
+            element={<Signup />}
+          >
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,8 +1,37 @@
 import React from 'react'
-
+import { useState } from 'react'
 const Signup = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSignup = (e) => {
+        e.preventDefault();
+        console.log("email:", email, "password:", password);
+
+    }
     return (
-        <div>Signup page</div>
+        <div>
+            <h2>Signup</h2>
+            <form onSubmit={handleSignup}>
+                <input
+                    type='email'
+                    placeholder='enter email'
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
+                />
+
+                <input
+                    type='password'
+                    placeholder='enter new password'
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                />
+
+                <button type='submit'>signup</button>
+            </form>
+        </div>
     )
 }
 
