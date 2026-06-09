2374e9e2b8332be99c56b2dd433ff87dca095b60
Ratan Mahata <ratanmahata116@gmail.com>
2026-06-10 01:06:27 +0530

    cors update

--- server/server.js
+++ server/server.js
@@ -10,12 +10,13 @@ const port = 3000;
 connectDB();
 
 app.use(express.json());
-app.use(cors(
-    {
-        origin: "https://blog-app-rust-omega-25.vercel.app",
-        credentials: true
-    }
-));
+app.use(cors({
+    origin: [
+        "http://localhost:5173",
+        "https://blog-app-rust-omega-25.vercel.app"
+    ],
+    credentials: true
+}));
 
 app.use(express.urlencoded({
     extended: true
